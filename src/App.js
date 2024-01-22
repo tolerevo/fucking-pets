c3b292adb138b90d17af3cf1e044d45c051bd512
tolerevo <tolerevo@hotmail.com>
2024-01-22 10:55:38 -0600

    initialize app router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
 // Add react-router-dom imports
-
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root/>}></Route>))
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
