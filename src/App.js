59c97f8e1446d830032c2dfa592ef6cb765a7d8f
tolerevo <tolerevo@hotmail.com>
2024-01-22 11:05:16 -0600

    add outlet and render home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ import {
   Route,
 } from "react-router-dom";
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root/>}></Route>))
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<HomePage />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
