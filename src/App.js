ccd7d7ea0d5c05606b23df454a7b27374ee4a30d
tolerevo <tolerevo@hotmail.com>
2024-01-22 11:27:53 -0600

    render multiple url

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const appRouter = createBrowserRouter(
     <Route path="/" element={<Root />}>
       <Route path=":type" element={<HomePage />} />
       <Route path="/" element={<HomePage />} />
+      <Route path="/:type/:id" element={<PetDetailsPage />} />
     </Route>
   )
 );
